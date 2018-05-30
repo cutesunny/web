bc2b38afeac29695e6c5e93c22c36a78cb3d5146
懒洋洋\猪头 <963126366@qq.com>
2018-05-30 20:46:41 +0800

    123456789

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,20 @@
+function login() {
+    $('#login').css('display', 'block');
+}
+function closelogin() {
+    $('#login').css('display', 'none');
+}
+function register() {
+    $('#register').css('display', 'block');
+}
+
+function closeregister() {
+    $('#register').css('display', 'none');
+}
+function certification() {
+    $('#certification').css('display', 'block');
+}
+
+function closecertification() {
+    $('#certification').css('display', 'none');
+}
