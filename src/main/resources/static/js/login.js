9511a19a07813baaf375905f27111142ba46647c
懒洋洋\猪头 <963126366@qq.com>
2018-06-01 11:29:00 +0800

    2018年6月1日11:28:53

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -18,3 +18,68 @@ function certification() {
 function closecertification() {
     $('#certification').css('display', 'none');
 }
+
+function doLogin() {
+    let username = $('#l_username').val();
+    let password = $('#l_password').val();
+    if(username !== null && username !== ''
+        && password !== null && password !== ''){
+        //do something
+        $.ajax({
+            url:'/api/user/login',
+            method:'post',
+            data:{
+                username:username,
+                password:password
+            },
+            dataType:'json',
+            success:function (response) {
+                if(response.code === 200){
+                    layer.msg('登录成功', {icon: 1}, function () {
+                        window.location.reload();
+                    });
+                }else{
+                    layer.msg(response.errMsg, {icon: 2});
+                }
+            },
+            error:function () {
+                layer.msg('登录失败', {icon: 2});
+            }
+        });
+    }
+}
+function doRegister() {
+    let username = $('#reg_username').val();
+    let password = $('#reg_password').val();
+    let email = $('#reg_email').val();
+    let phone = $('#reg_phone').val();
+    let type = $('input[name="reg_type"]:checked').val();
+    if(username !== null && username !== ''
+        && password !== null && password !== ''){
+        //do something
+        $.ajax({
+            url:'/api/user/register',
+            method:'post',
+            data:{
+                username:username,
+                password:password,
+                email:email,
+                phone:phone,
+                type:type
+            },
+            dataType:'json',
+            success:function (response) {
+                if(response.code === 200){
+                    layer.msg('注册成功', {icon: 1}, function () {
+                        window.location.reload();
+                    });
+                }else{
+                    layer.msg(response.errMsg, {icon: 2});
+                }
+            },
+            error:function () {
+                layer.msg('注册失败', {icon: 2});
+            }
+        });
+    }
+}
